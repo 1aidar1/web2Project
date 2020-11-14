e372165f4320f0c0c2ecc77ead21954e67b08b58
Aidarkhan Arkayev <aidar22222222@gmail.com>
2020-11-13 16:18:24 -0800

    Initial Commit

---
+++ controller.php
@@ -0,0 +1,80 @@
+<?php
+
+
+require('model.php');
+
+$isSigned = false;
+$display_type = 'no-signin';
+$error_msg_handle_signin = '';
+$error_msg_password_signin = '';
+$error_msg_UIN_register = '';
+$error_msg_handle_register = '';
+$error_msg_tel_register = '';
+$error_msg_password_register = '';
+
+$UIN = '';
+$handle = '';
+$password = '';
+$tel = '';
+
+if (empty($_POST['page'])) {
+    include('MainPage.php');
+} else {
+    $page = $_POST['page'];
+    if ($page == 'MainPage') {
+        $command = $_POST['command'];
+        switch ($command) {
+            case 'SignIn':
+                if(isset($_POST['handle'])){
+                    $handle = $_POST['handle'];
+                }
+                if(isset($_POST['password'])){
+                    $password = $_POST['password'];
+                }
+                if (check_validity($handle, $password)) {
+                    $isSigned = true;
+                    $display_type = 'signed';
+                } else {
+                    $isSigned = false;
+                    $display_type = 'sign-in';
+                    $error_msg_handle_signin = 'Wrong handle, or';
+                    $error_msg_password_signin = 'Wrong password';
+                }
+                $ans = array(
+                    "isSigned" => $isSigned,
+                    "display" => $display_type,
+                    "error_msg_handle_signin" => $error_msg_handle_signin,
+                    "error_msg_password_signin" => $error_msg_password_signin,
+                    "error_msg_UIN_register" => $error_msg_UIN_register,
+                    "error_msg_handle_register" => $error_msg_handle_register,
+                    "error_msg_tel_register" => $error_msg_tel_register,
+                    "error_msg_password_register" => $error_msg_password_register
+                );
+                echo json_encode($ans);
+                break;
+            case 'Register':
+                $UIN = $_POST['UIN'];
+                $handle = $_POST['handle'];
+                $password = $_POST['password'];
+                $tel = $_POST['tel'];
+                if (join_a_user($UIN, $handle, $password, $tel)) {
+                    $display_type = 'no-sign-in';
+                } else {
+                    $display_type = 'register';
+                    $error_msg_UIN_register = 'Error';
+                }
+                $ans = array(
+                    "isSigned" => $isSigned,
+                    "display" => $display_type,
+                    "error_msg_handle_signin" => $error_msg_handle_signin,
+                    "error_msg_password_signin" => $error_msg_password_signin,
+                    "error_msg_UIN_register" => $error_msg_UIN_register,
+                    "error_msg_handle_register" => $error_msg_handle_register,
+                    "error_msg_tel_register" => $error_msg_tel_register,
+                    "error_msg_password_register" => $error_msg_password_register
+                );
+                echo json_encode($ans);
+                break;
+        }
+    }
+}
---
+++ model.php
@@ -0,0 +1,72 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'aarkayevf20', 'aezakmi0101', 'C354_aarkayevf20');
+
+function check_existence_handle($handle)
+{
+    global $conn;
+    $sql = "select Handle from ProjectUsers where Handle = '$handle'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0)
+        return true;
+    else
+        return false;
+}
+function check_existence_UIN($UIN)
+{
+    global $conn;
+    $sql = "select UIN from ProjectUsers where UIN = '$UIN'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0)
+        return true;
+    else
+        return false;
+}
+
+function get_user_id($handle)
+{
+    global $conn;
+    $sql = "select ID,Handle from ProjectUsers where Handle = '$handle'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['ID'];
+    } else
+        return -1;
+}
+
+function check_validity($handle, $password)
+{
+    global $conn;
+    $sql = "select Handle,Password from ProjectUsers where Handle = '$handle' and Password ='$password'";
+
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) > 0)
+        return true;
+    else
+        return false;
+}
+function isTel($tel){
+    $tel = str_replace(' ','',$tel);;
+     if($tel[0]=='1' && strlen($tel)==11 && is_numeric($tel)==1){
+         return true;
+     }
+     else{
+         return false;
+     }
+}
+
+function join_a_user($UIN, $handle, $password, $tel)
+{
+    global $conn;
+    $current_date = date('Ymd');
+    if (check_existence_handle($handle) || check_existence_UIN($UIN) || !isTel($tel)) {
+        return false;
+    } else {
+        $sql = "insert into ProjectUsers value (NULL,'$UIN','$handle','$password', '$tel','$current_date')";
+        if (mysqli_query($conn, $sql)) {
+            return true;
+        } else return false;
+    }
+}
+?>
