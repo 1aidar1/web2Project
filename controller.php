74dbe3fe1afcee146bde13dd844c18d327aca6c7
Aidarkhan Arkayev <aidar22222222@gmail.com>
2020-11-14 05:08:52 -0800

    https

--- controller.php
+++ controller.php
@@ -1,5 +1,10 @@
 <?php
 
+if(!isset($_SERVER['HTTPS'])){
+    $url = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header("Location: " . $url);
+    exit;
+}
 
 require('model.php');
 
