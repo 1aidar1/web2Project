9100cb1c8c8321a69e1091b362fab0b8edf2d821
Aidarkhan Arkayev <aidar22222222@gmail.com>
2020-11-15 09:54:38 -0800

    change setting v1

--- controller.php
+++ controller.php
@@ -7,6 +7,7 @@ if(!isset($_SERVER['HTTPS'])){
 }
 
 require('model.php');
+require('model2.php');
 
 $isSigned = false;
 $display_type = 'no-signin';
@@ -21,13 +22,37 @@ $UIN = '';
 $handle = '';
 $password = '';
 $tel = '';
+$id = -1;
+
+$page = NULL;
+$command = NULL;
+
+if (isset($_SESSION['handle'])){
+    if(check_validity($_SESSION['handle'],$_SESSION['password'] )) {
+        $handle = $_SESSION['handle'];
+        $password = $_SESSION['password'];
+        $isSigned = true;
+        $display_type = 'no-sign-in';
+        $id = get_user_id($handle);
+        $ans = array(
+            "isSigned" => $isSigned,
+            "display" => $display_type
+        );
+        echo json_encode($ans);
+        exit();
+    } 
+}//TODO: signin onload page
+
+
 
 if (empty($_POST['page'])) {
     include('MainPage.php');
 } else {
     $page = $_POST['page'];
     if ($page == 'MainPage') {
-        $command = $_POST['command'];
+        if(isset($_POST['command'])){
+            $command = $_POST['command'];
+        }
         switch ($command) {
             case 'SignIn':
                 if(isset($_POST['handle'])){
@@ -37,8 +62,12 @@ if (empty($_POST['page'])) {
                     $password = $_POST['password'];
                 }
                 if (check_validity($handle, $password)) {
+                    
+                    $_SESSION['handle'] = $handle;
+                    $_SESSION['password'] = $password;
                     $isSigned = true;
                     $display_type = 'no-sign-in';
+                    $id = get_user_id($handle);
                 } else {
                     $isSigned = false;
                     $display_type = 'sign-in';
@@ -83,6 +112,34 @@ if (empty($_POST['page'])) {
         }
     }
     else if($page == 'UserPage'){
-
+        if(isset($_POST['command'])){
+            $command = $_POST['command'];
+        }
+        switch($command){
+            case 'ChangeUIN':
+                if(isset($_POST['new_UIN'])){
+                    $UIN = $_POST['new_UIN'];
+                }
+                echo change_UIN($UIN,$id);
+                break;
+            case 'ChangeHandle':
+                if(isset($_POST['new_handle'])){
+                    $handle = $_POST['new_handle'];
+                }
+                echo change_handle($handle,$id);
+                break;
+            case 'ChangePassword':
+                if(isset($_POST['new_password'])){
+                    $password = $_POST['new_password'];
+                }
+                echo change_UIN($password,$id);
+                break;
+            case 'ChangeTel':
+                if(isset($_POST['new_tel'])){
+                    $tel= $_POST['new_tel'];
+                }
+                echo change_tel($tel,$id);
+                break;
+        }
     }
 }
---
+++ model2.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_connect('localhost', 'aarkayevf20', 'aezakmi0101', 'C354_aarkayevf20');
+
+function change_UIN($UIN,$id){
+    global $conn;
+    $sql = "update ProjectUsers Set UIN = '$UIN' where ID = '$id'";
+    return mysqli_query($conn,$sql); 
+}
+function change_handle($handle,$id){
+    global $conn;
+    $sql = "update ProjectUsers Set Handle = '$handle' where ID = '$id'";
+    return mysqli_query($conn,$sql); 
+}
+function change_password($password,$id){
+    global $conn;
+    $sql = "update ProjectUsers Set Handle = '$password' where ID = '$id'";
+    return mysqli_query($conn,$sql); 
+}
+function change_tel($tel,$id){
+    global $conn;
+    $sql = "update ProjectUsers Set Handle = '$tel' where ID = '$id'";
+    return mysqli_query($conn,$sql); 
+}
+?>
